0881c4aaf5a47ef00caa6a1f15a8695b8e0c57f2
Ben Solum <benjamin@soluml.com>
2017-03-08 20:51:11 -0600

    Update

--- demo/src/js/app/components/wrapper.jsx
+++ demo/src/js/app/components/wrapper.jsx
@@ -1,10 +1,12 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
 
 const Main = class extends React.Component {
   render() {
     const attrs = {
       main: {
         className: styled`
+          background: #000 url(../img/space.jpg) no-repeat;
+          background-size: cover;
           bottom: 0;
           left: 0;
           position: absolute;
@@ -12,23 +14,123 @@ const Main = class extends React.Component {
           top: 0;
         `
       },
+      canvas: {
+        className: styled`
+          bottom: 0;
+          left: 0;
+          opacity: 0.2;
+          position: absolute;
+          right: 0;
+          top: 0;
+        `,
+        ref: (node) => { this.canvas = node; }
+      },
       h1: {
         className: styled`
+          left: 50%;
+          position: absolute;
           text-align: center;
+          top: 50%;
+          transform: translate(-50%, -50%);
         `
-      }
+      },
     };
 
+    const bibleVerse = ['Now', 'the', 'earth', 'was', 'formless', 'and', 'empty…', 'Genesis 1:2'];
+
     return (
-      <div>
-        <main {...attrs.main}>
-          <h1 {...attrs.h1}>Welcome to my demo!</h1>
-        </main>
-      </div>
+      <main {...attrs.main}>
+        <canvas {...attrs.canvas} />
+        <h1 {...attrs.h1}>
+          {
+            bibleVerse.map(word => <span key={word}>
+              {word}
+            </span>)
+          }
+        </h1>
+      </main>
     );
   }
-};
+  componentDidMount() {
+    //These pens were used for inspiration and/or plagiarism depending on your conscience
+    //http://codepen.io/RobinTreur/pen/pyWLeB
+    //http://codepen.io/apsolut/pen/EhmeL
+    //http://codepen.io/wheately/pen/VLjgyR?editors=0010
+    const canvas = this.canvas;
+    const context = canvas.getContext('2d');
+    const particles = [];
 
-Main.propTypes = {};
+    function drawCanvas() {
+      context.clearRect(0, 0, canvas.width, canvas.height);
+      context.fillStyle = 'hsla(332, 95%, 5%, 1)';
+      context.fillRect(0, 0, canvas.width, canvas.height);
+    }
+
+    function draw() {
+      drawCanvas();
+
+      for (let i = 0; i < particles.length; i++) {
+        if (particles[i].dead) particles.splice(i, 1);
+
+        context.globalCompositeOperation = 'lighter';
+        context.beginPath();
+
+        const gradient = context.createRadialGradient(particles[i].x, particles[i].y, 0, particles[i].x, particles[i].y, particles[i].size);
+        gradient.addColorStop(1, 'transparent');
+        gradient.addColorStop(0, particles[i].color);
+
+        context.fillStyle = gradient;
+        context.arc(particles[i].x, particles[i].y, particles[i].size, 0, Math.PI * 2, true);
+        context.closePath();
+        context.fill();
+
+        //Do gravity stuff.
+        particles[i].x += particles[i].Hvelocity;
+        particles[i].y += particles[i].Vvelocity;
+
+        if (particles[i].x - particles[i].size > canvas.width || particles[i].y - particles[i].size > canvas.height) particles[i].dead = true;
+      }
+
+      if (particles.length < 1000) {
+        for (let i = 0; i < 10; i++) generateParticle();
+      }
+
+      requestAnimationFrame(draw);
+    }
+
+    function init() {
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
+      context.globalCompositeOperation = 'lighten';
+
+      drawCanvas();
+
+      for (let i = 0; i < 50; i++) generateParticle();
+
+      draw();
+    }
+
+    function generateParticle() {
+      const colors = [
+        'rgba(142, 68, 173,0.3)',
+        'rgba(41, 128, 185, 0.3)',
+      ];
+      const color = Math.floor(Math.random() * colors.length) + 0;
+      const particle = {
+        x: Math.floor(Math.random() * -100) + 0,
+        y: Math.floor(Math.random() * -100) + 0,
+        size: Math.floor(Math.random() * 150) + 30,
+        color: colors[color],
+        Hvelocity: Math.floor(Math.random() * 10) + 1,
+        Vvelocity: Math.floor(Math.random() * 10) + 1,
+        dead: false
+      };
+
+      particles.push(particle);
+    }
+
+    init();
+  }
+};
 
 export default Main;
